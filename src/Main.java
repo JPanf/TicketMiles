5722d806f8389d2e641255742302f617a107ed5f
Julia <julia.panfilova@mail.ru>
2022-07-05 21:48:17 +0300

    Code formated

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной милли
-
         int ticketAmount = 12385;
         int mile = 20;
         int bonus;
 
-        // Рассчитываете количество бонусных милль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
-
-                if (ticketAmount >= 0) {
-                    bonus = ticketAmount / mile;
-                } else {
-                    bonus = 0;
-                }
+        if (ticketAmount >= 0) {
+            bonus = ticketAmount / mile;
+        } else {
+            bonus = 0;
+        }
 
         System.out.println(bonus + " бонусных(ая) миль(я)");
     }
