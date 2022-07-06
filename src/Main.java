9560439cd51c760ffe687493bce805b0425d339e
Julia <julia.panfilova@mail.ru>
2022-07-06 22:12:53 +0300

    Calc class added

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticketAmount = 12385;
-        int mile = 20;
-        int bonus;
-
-        if (ticketAmount >= 0) {
-            bonus = ticketAmount / mile;
-        } else {
-            bonus = 0;
-        }
-
-        System.out.println(bonus + " бонусных(ая) миль(я)");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
